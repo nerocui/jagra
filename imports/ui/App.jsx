826efa6640dd5b2fd8621d5a14cfd30e83434b2e
Nero Cui <nerocui@outlook.com>
2019-03-26 10:18:28 -0700

    clean up

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import Hello from "./Hello.jsx";
 import Info from "./Info.jsx";
 
-const App = () => (
-	<div>
-		<h1>Welcome to Meteor!</h1>
-		<Hello />
-		<Info />
-	</div>
-);
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: 'Jagra',
+		};
+	}
+
+	render() {
+		const { title } = this.state;
+		return (
+			<div>
+				<h1>Welcome to {title}!</h1>
+				<Hello />
+				<Info />
+			</div>
+		);
+	}
+}
 
 export default App;
