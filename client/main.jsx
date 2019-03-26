ed76bd2845ea0def79c5bd9ea132f7d58aee9437
Nero Cui <nerocui@outlook.com>
2019-03-26 07:05:40 -0700

    clean up

--- client/main.jsx
+++ client/main.jsx
@@ -4,6 +4,5 @@ import { render } from 'react-dom';
 import App from '/imports/ui/App'
 
 Meteor.startup(() => {
-  var something = 1;
   render(<App />, document.getElementById('react-target'));
 });
