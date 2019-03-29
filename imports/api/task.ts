6cf65574aa53554978f21dcaaaf7b960c2f714f0
Nero Cui <nerocui@outlook.com>
2019-03-29 00:44:31 -0700

    added few more methods and git ignored TODO plug

--- imports/api/task.ts
+++ imports/api/task.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import { Meteor } from "meteor/meteor";
-import { Tasks } from "./db.js";
+import { Tasks, Employees, Teams } from "./db.js";
 import Status from "../constant/status";
 import moment from 'moment';
 
@@ -18,11 +18,10 @@ import moment from 'moment';
 	// private relationshipsId: Array<String>;
 	// private teamId: String;
 Meteor.methods({
-	'tasks.insert'(title: String, description: String) {
+	"tasks.insert"(title: String, description: String) {
 		if (!this.userId) {
 			throw new Meteor.Error('not-authorized');
 		}
-
 		return Tasks.insert({
 			title,
 			description,
@@ -36,7 +35,46 @@ Meteor.methods({
 			watchersId: [this.userId],
 			relationshipsId: [],
 			teamId: [],
+		}, (err, task) => {
+			if (err) {
+				throw new Meteor.Error('Failed to create task');
+			}
+			// TODO({hiSyl9OaS}): send out email about this update
+			// TODO({U-aaiJmDa}): add this to history log in admin data
 		});
 	},
-
+	"tasks.updateDescription"(_id: String, description: String) {
+		if (!this.userId) {
+			throw new Meteor.Error("not-authorized");
+		}
+		let task = Tasks.findOne({_id}).fetch();
+		if (!(this.userId === task.creatorId || this.userId === task.assigneeId)) {
+			throw new Meteor.Error("no privilege");
+		}
+		Tasks.update(_id, {description});
+		// TODO({yV-k71J7G}): send out email about this update
+		// TODO({X_74Ud483}): add this to history log in admin data
+	},
+	"tasks.updateStatus"(_id: String, status: String) {
+		if (!this.userId) {
+			throw new Meteor.Error("not-authorized");
+		}
+		let task = Tasks.findOne({_id}).fetch();
+		if (!(this.userId === task.creatorId || this.userId === task.assigneeId)) {
+			throw new Meteor.Error("no privilege");
+		}
+		Tasks.update(_id, {status});
+		// TODO({JL8QmmMYU}): send out email about this update
+		// TODO({CeBL8z7CB}): add this to history log in admin data
+	},
+	"tasks.assignTo"(_id: String, assigneeId: String) {
+		if (!this.userId) {
+			throw new Meteor.Error("not-authorized");
+		}
+		let task = Tasks.findOne({_id}).fetch();
+		if (!(this.userId === task.creatorId || this.userId === task.assigneeId)) {
+			throw new Meteor.Error("no privilege");
+		}
+		//TODO({22u7JpQt2}): Finish implimentation
+	}
 });
