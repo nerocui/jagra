dd1ae24d9238ef7d778ce8eb794f934fb8dc6079
Nero Cui <nerocui@outlook.com>
2019-04-01 15:01:31 -0700

    added utils, changed lint rule, installed fast-deep-equal for safe object compare.

--- imports/api/task.ts
+++ imports/api/task.ts
@@ -1,8 +1,9 @@
 // @ts-ignore
 import { Meteor } from "meteor/meteor";
-import { Tasks, Employees, Teams } from "./db.js";
+import { Tasks, Employees, Teams } from "./db";
 import Status from "../constant/status";
 import moment from 'moment';
+import { removeElement, addToList } from '../util/arrayUtil';
 
 	// private _id: String;
 	// private title: String;
@@ -67,13 +68,36 @@ Meteor.methods({
 		// TODO({JL8QmmMYU}): send out email about this update
 		// TODO({CeBL8z7CB}): add this to history log in admin data
 	},
-	"tasks.assignTo"(_id: String, assigneeId: String) {
+	"tasks.assignTo"(_id: String, employeeId: String, assigneeId: String) {
 		if (!this.userId) {
 			throw new Meteor.Error("not-authorized");
 		}
 		let task = Tasks.findOne({_id}).fetch();
 		if (!(this.userId === task.creatorId || this.userId === task.assigneeId)) {
 			throw new Meteor.Error("no privilege");
+		}
+		if (this.userId === task.creatorId) {
+			const manager = Employees.findOne({employeeId}).fetch(),
+				employees = [...manager.employeesId],
+				teamId = manager.teamId,
+				team = Teams.findOne({employeeId: teamId}).fetch(),
+				teamMembers = [...team.members];
+			if (employees.includes(assigneeId) || teamMembers.includes(assigneeId)) {
+				const currentAssigneeId = task.assignee,
+					currentAssignee = Employees.findOne({_id: currentAssigneeId}).fetch(),
+					currentAssigneeTodos = currentAssignee.tasksToDo,
+					assignee = Employees.findOne({_id: assigneeId}).fetch();
+				//remove the task from current assignee's todo list
+				Employees.update({_id: currentAssigneeId}, {tasksToDo: removeElement(currentAssigneeTodos, _id)});
+				//add the task to the target todo list
+				Employees.update({_id}, {tasksToDo: addToList(assignee.tasksToDo, _id)});
+				//change the assignee value from task db to the new assignee
+				Tasks.update({_id}, {assignee: assigneeId});
+			} else {
+				
+			}
+		} else {
+
 		}
 		//TODO({22u7JpQt2}): Finish implimentation
 	}
