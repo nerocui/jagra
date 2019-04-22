68f5a3ef3d3e596cbd342da51525195049115465
Nero Cui <neroc@ensemble.com>
2019-04-22 16:26:12 -0700

    finished implementation on task and comment template
    next step: implement the log api, implement email api, make email and log configurable via config based on client price tier.

--- imports/templates/impl/task_template.js
+++ imports/templates/impl/task_template.js
@@ -1,7 +1,7 @@
 import Template from "../interfaces/template";
 import TYPE from "../../constant/actions/type";
 
-class CreateTask extends Template {
+class Create extends Template {
 	constructor(taskId, creatorId) {
 		super(TYPE.CREATE_TASK);
 		this.taskId = taskId;
@@ -11,16 +11,60 @@ class CreateTask extends Template {
 	//like color of each field, template text etc.
 }
 
-class DeleteTask extends Template {
+class Delete extends Template {
 	constructor(taskId) {
 		super(TYPE.DELETE_TASK);
 		this.taskId = taskId;
 	}
 }
 
+class ModifyDescription extends Template {
+	constructor(taskId, modifierId, oldDes, newDes) {
+		super(TYPE.MODIFY_TASK_DES);
+		this.taskId = taskId;
+		this.modifierId = modifierId;
+		this.oldDes = oldDes;
+		this.newDes = newDes;
+	}
+}
+
+class ModifyStatus extends Template {
+	constructor(taskId, modifierId, oldStatus, newStatus) {
+		super(TYPE.MODIFY_TASK_STATUS);
+		this.taskId = taskId;
+		this.modifierId = modifierId;
+		this.oldStatus = oldStatus;
+		this.newStatus = newStatus;
+	}
+}
+
+class ModifyDue extends Template {
+	constructor(taskId, modifierId, oldDue, newDue) {
+		super(TYPE.MODIFY_TASK_DUE);
+		this.taskId = taskId;
+		this.modifierId = modifierId;
+		this.oldDue = oldDue;
+		this.newDue = newDue;
+	}
+}
+
+class Assign extends Template {
+	constructor(taskId, oldAssigneeId, newAssigneeId, assignerId) {
+		super(TYPE.ASSIGN_TASK);
+		this.taskId = taskId;
+		this.oldAssigneeId = oldAssigneeId;
+		this.newAssigneeId = newAssigneeId;
+		this.assignerId = assignerId;
+	}
+}
+
 //TODO: add more class for each type of action, and add more files for more action types
 
 export default {
-	CreateTask,
-	DeleteTask,
+	Create,
+	Delete,
+	ModifyDescription,
+	ModifyStatus,
+	ModifyDue,
+	Assign,
 };
