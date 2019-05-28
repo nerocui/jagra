ab2c6d0ab0d679fed24444865864dab637f36e01
Nero <nerocui@outlook.com>
2019-05-28 01:21:00 -0700

    added login, signup, admin check to auth util.

---
+++ imports/config/client/prod.js
@@ -0,0 +1,6 @@
+export default {
+	COMPANY_NAME: process.env.COMPANY_NAME,
+	ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+	ADMIN_NAME: process.env.ADMIN_NAME,
+	ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+};
--- imports/util/authUtil.js
+++ imports/util/authUtil.js
@@ -1,9 +1,39 @@
 import { Meteor } from "meteor/meteor";
 import ROLE from "../constant/role";
+import EMPLOYEESAPI from "../constant/methods/employeesAPI";
+import ADMIN_INFO from "../config/client/conf";
+import { Employees } from "../api/db";
 
 export const isAuthenticated = () => Meteor.userId || Meteor.isTest;
 
 export const isAdmin = employeeDb => {
 	const user = employeeDb.findOne({ _id: Meteor.userId });
 	return user.role === ROLE.ADMIN || Meteor.isTest;
+};
+
+export const login = (email, password, callback) => {
+	const accountId = Meteor.loginWithPassword(email, password, err => {
+		callback(err);
+	});
+	const employee = Employees.findOne({ accountId });
+	if (employee && employee.role === ROLE.ADMIN) {
+		//TODO: Do something here for redirection
+	}
+};
+
+export const signup = (email, password, firstName, lastName, callback) => {
+	const accountId = Meteor.createUser(email, password, err => {
+		callback(err);
+	});
+	Meteor.call(EMPLOYEESAPI.INSERT, accountId, email, firstName, lastName, ROLE.EMPLOYEE);
+};
+
+export const adminCheck = db => {
+	const admin = db.findOne({ role: ROLE.ADMIN });
+	if (!admin) {
+		const newAdmin = Meteor.createUser(ADMIN_INFO.ADMIN_EMAIL, ADMIN_INFO.ADMIN_PASSWORD, err => {
+			console.log("Fail to create admin", err);
+		});
+		Meteor.call(EMPLOYEESAPI.INSERT, newAdmin, ADMIN_INFO.ADMIN_EMAIL, "admin", ADMIN_INFO.COMPANY_NAME, ROLE.ADMIN);
+	}
 };
