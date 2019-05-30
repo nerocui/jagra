73c91b65f619d200ef45f589227d371ed4895415
Annie Wang <annie.wang@hootsuite.com>
2019-05-29 21:23:28 -0700

    edited by eslint rule

--- imports/api/employee.js
+++ imports/api/employee.js
@@ -1,209 +1,234 @@
-import {Meteor} from "meteor/meteor";
-import {Employees} from "./db";
+import { Meteor } from "meteor/meteor";
+import { Employees } from "./db";
 import EMPLOYEESAPI from "../constant/methods/employeesAPI";
 import { isAuthenticated } from "../util/authUtil";
-import {AuthError, EmployeeError} from "../constant/error";
+import { AuthError, EmployeeError } from "../constant/error";
 import TASKSAPI from "../constant/methods/tasksAPI";
-import {EmployeeMessage} from "../constant/message";
+import { EmployeeMessage } from "../constant/message";
+import {
+	removeElement,
+	addToList,
+	removeAllFromList,
+} from "../util/arrayUtil";
 
 if (Meteor.isServer) {
-    Meteor.publish("employees", () => Employees.find());
+	Meteor.publish("employees", () => Employees.find());
 }
 
 // username, password, accountId, employeeId
 export const insertEmployee = (db, employeeId, firstName, lastName, role) => {
-    if (!isAuthenticated()) {
-        throw new Meteor.Error(AuthError.NOT_AUTH);
-    }
-    return db.insert({
-        employeeId, firstName, lastName,
-        // not sure onBoard should be a today or not
-        onBoard: today.getDate(),
-        // should manager id and team id be null at the very begining when meteor doing auth
-        // or it might come with assigned managerId while inserting employee??
-        // I will keep it null for now
-        managerId: null,
-        teamId: null,
-        tasksAssienedId: [],
-        tasksCreatedId: [],
-        tasksWatchingId: [],
-        requestsSentId: [],
-        requestReceivedId: [],
-        commentsId: [],
-        filesId: [],
-        employeesId: [],
-        individualsId: [],
-        teamsId: [],
-        // from my understanding, role should be one of"employer", "employee", "admin"?
-        // i think it would be better to have it once we create employee, since it make sense to determine the level of the account at the time we create it
-        role,
-    }, (err, firstName, lastName) => {
-        if (err) {
-            throw new Meteor.Error(EmployeeError.EMPLOYEE_INSERT_FAIL);
-        } else {
-            console.log(`${
-                EmployeeMessage.EMPLOYEE_CREATED
-            } ${firstName} ${lastName}`);
-        }
-    })
+	if (!isAuthenticated()) {
+		throw new Meteor.Error(AuthError.NOT_AUTH);
+	}
+	let today = new Date();
+	const dd = String(today.getDate()).padStart(2, "0");
+	const mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+	const yyyy = today.getFullYear();
+
+	today = `${ mm }/${ dd }/${ yyyy }`;
+	return db.insert(
+		{
+			employeeId,
+			firstName,
+			lastName,
+			// not sure onBoard should be a today or not
+			onBoard: today,
+			// should manager id and team id be null at the very begining when meteor doing auth
+			// or it might come with assigned managerId while inserting employee??
+			// I will keep it null for now
+			managerId: null,
+			teamId: null,
+			tasksAssienedId: [],
+			tasksCreatedId: [],
+			tasksWatchingId: [],
+			requestsSentId: [],
+			requestReceivedId: [],
+			commentsId: [],
+			filesId: [],
+			employeesId: [],
+			individualsId: [],
+			teamsId: [],
+			// from my understanding, role should be one of"employer", "employee", "admin"?
+			// i think it would be better to have it once we create employee, since it make sense to determine the level of the account at the time we create it
+			role,
+		},
+		(err, _id) => {
+			if (err) {
+				throw new Meteor.Error(EmployeeError.EMPLOYEE_INSERT_FAIL);
+			} else {
+				console.log(
+					`${
+						EmployeeMessage.EMPLOYEE_CREATED
+					} with employee id ${ _id }`,
+				);
+			}
+		},
+	);
 };
 
 export const removeEmployee = (db, _id) => {
-    if (!isAuthenticated()) {
-        throw new Meteor.Error(AuthError.NOT_AUTH);
+	if (!isAuthenticated()) {
+		throw new Meteor.Error(AuthError.NOT_AUTH);
 	}
-	//TODO: not too sure use _id or employeeid
-    const employee = Employees.findOne({_id}), {employeeId} = employee;
-    if (! employee) {
-        throw new Meteor.Error(EmployeeError.EMPLOYEE_NOT_EXIST);
-    }
-    if (employeeId !== this.userId) {
-        throw new Meteor.Error(AuthError.NO_PRIVILEGE);
-    }
-
-    // TODO:
-    // need to make sure tasks status associated with this employee has been updated 
-    return db.remove({
-        _id
-    }, err => {
-        if (err) {
-            throw new Meteor.Error(EmployeeError.EMPLOYEE_REMOVE_FAIL);
-        }
-    });
-};
-
-//TODO: do we need this in employee???
-//I think so, delete this for now :/
-// export const createTask = (db, employeeId, taskId, taskTitle, taskDescription) => {
-//     if (!this.userId) {
-//         throw new Meteor.Error(AuthError.NOT_AUTH);
-// 	}
-// 	//TODO: not too sure use _id or employeeid
-// 	const employee = db.findOne({ employeeId });
-// 	if (!employee) {
-// 		throw new Meteor.Error(EmployeeError.EMPLOYEE_NOT_EXIST);
-// 	}
-// 	let { tasksCreatedId } = employee;
-// 	tasksCreatedId = [...tasksCreatedId];
-// 	if (!tasksCreatedId.includes(taskId)) {
-// 		tasksCreatedId = addToList(tasksCreatedId, taskId);
-// 		return db.update({ employeeId }, { tasksCreatedId }, err => {
-// 			if (err) {
-// 				throw new Meteor.Error(EmployeeError.TASK_NOT_CREATABLE);
-// 			} else {
-// 				//TODO:
-// 			}
-// 		});
-// 	}
-    
-//     return db.update(_id, {tasksCreatedId })
-// };
-
-export const removeCreateTaskFromEmployee = (db, employeeId, taskId) => {
-    if (!isAuthenticated()) {
-        throw new Meteor.Error(AuthError.NOT_AUTH);
-	}
-	
-	const employee = db.findOne({ employeeId });
+	// TODO: not too sure use _id or employeeid
+	const employee = Employees.findOne({ _id }),
+		{ employeeId } = employee;
 	if (!employee) {
 		throw new Meteor.Error(EmployeeError.EMPLOYEE_NOT_EXIST);
 	}
-	let {tasksAssienedId} = employee;
-	tasksAssienedId = [...tasksAssienedId];
-	if(tasksCreatedId.includes(taskId)){
-		tasksAssienedId = removeElement(tasksAssienedId, taskId);
-		return db.update({ employeeId }, { tasksAssienedId }, err => {
-			if (err) {
-				throw new Meteor.Error(EmployeeError.CREATED_TASK_NOT_REMOVABLE);
-			} else {
-				//TODO:
-			}
-		});
+	if (_id !== this.userId) {
+		throw new Meteor.Error(AuthError.NO_PRIVILEGE);
 	}
 
-    // TODO:
-
-
+	// TODO:
+	// need to make sure tasks status associated with this employee has been updated
+	return db.remove(
+		{
+			employeeId,
+		},
+		err => {
+			if (err) {
+				throw new Meteor.Error(EmployeeError.EMPLOYEE_REMOVE_FAIL);
+			}
+		},
+	);
 };
 
-export const removeAssignedTaskFromExployee = (db, employeeId, taskId) => {
-    if (!isAuthenticated()) {
-        throw new Meteor.Error(AuthError.NOT_AUTH);
-    }
+export const createTask = (db, _id, taskId, taskTitle, taskDescription) => {
+	if (!this.userId) {
+		throw new Meteor.Error(AuthError.NOT_AUTH);
+	}
+	// TODO: not too sure use _id or employeeid
+	const employee = db.findOne({ _id });
+	if (!employee) {
+		throw new Meteor.Error(EmployeeError.EMPLOYEE_NOT_EXIST);
+	}
+	let { tasksCreatedId } = employee;
+	tasksCreatedId = [...tasksCreatedId];
+	if (!tasksCreatedId.includes(taskId)) {
+		tasksCreatedId = addToList(tasksCreatedId, taskId);
+		return db.update(
+			{
+				_id,
+			},
+			{
+				tasksCreatedId,
+			},
+			err => {
+				if (err) {
+					throw new Meteor.Error(EmployeeError.TASK_NOT_CREATABLE);
+				} else {
+					// TODO:
+				}
+			}
+		);
+	}
 
-    // TODO:
-    
-
+	return db.update(_id, { tasksCreatedId });
 };
 
-export const removeWatchedTaskFromEmployee = (db, employeeId, taskId) => {
-    if (!isAuthenticated()) {
-        throw new Meteor.Error(AuthError.NOT_AUTH);
-    }
+export const removeCreateTaskFromEmployee = (db, _id, taskId) => {
+	if (!isAuthenticated()) {
+		throw new Meteor.Error(AuthError.NOT_AUTH);
+	}
 
-    // TODO:
-    
+	const employee = db.findOne({ _id });
+	if (!employee) {
+		throw new Meteor.Error(EmployeeError.EMPLOYEE_NOT_EXIST);
+	}
+	let { tasksCreatedId } = employee;
+	tasksCreatedId = [...tasksCreatedId];
+	if (tasksCreatedId.includes(taskId)) {
+		tasksCreatedId = removeElement(tasksCreatedId, taskId);
+		return db.update(
+			{
+				_id,
+			},
+			{
+				tasksCreatedId,
+			},
+			err => {
+				if (err) {
+					throw new Meteor.Error(
+						EmployeeError.CREATED_TASK_NOT_REMOVABLE,
+					);
+				} else {
+					// TODO:
+				}
+			},
+		);
+	}
 
+	// TODO:
 };
 
-export const assignTaskToEmployee = (db, employeeId, taskId) => {
-    if (!isAuthenticated()) {
-        throw new Meteor.Error(AuthError.NOT_AUTH);
-    }
+export const removeAssignedTaskFromExployee = (db, _id, taskId) => {
+	if (!isAuthenticated()) {
+		throw new Meteor.Error(AuthError.NOT_AUTH);
+	}
 
-    // TODO:
-    
+	// TODO:
 };
 
-export const watchTask = (db, employeeId, taskId) => {
-    if (!isAuthenticated()) {
-        throw new Meteor.Error(AuthError.NOT_AUTH);
-    }
+export const removeWatchedTaskFromEmployee = (db, _id, taskId) => {
+	if (!isAuthenticated()) {
+		throw new Meteor.Error(AuthError.NOT_AUTH);
+	}
 
-    // TODO:
-   
+	// TODO:
 };
 
-export const unwatchTask = (db, employeeId, taskId) => {
-    if (!isAuthenticated()) {
-        throw new Meteor.Error(AuthError.NOT_AUTH);
-    }
+export const assignTaskToEmployee = (db, _id, taskId) => {
+	if (!isAuthenticated()) {
+		throw new Meteor.Error(AuthError.NOT_AUTH);
+	}
 
-    // TODO:
+	// TODO:
+};
 
+export const watchTask = (db, _id, taskId) => {
+	if (!isAuthenticated()) {
+		throw new Meteor.Error(AuthError.NOT_AUTH);
+	}
+
+	// TODO:
+};
+
+export const unwatchTask = (db, _id, taskId) => {
+	if (!isAuthenticated()) {
+		throw new Meteor.Error(AuthError.NOT_AUTH);
+	}
+
+	// TODO:
 };
 
 Meteor.methods({
-	[EMPLOYEESAPI.INSERT](employeeId, firstName, lastName, role) {
-		return insertEmployee(Employees, employeeId, firstName, lastName, role);
+	[EMPLOYEESAPI.INSERT](_id, firstName, lastName, role) {
+		return insertEmployee(Employees, _id, firstName, lastName, role);
 	},
-	//TODO: ADDING parameters to the method mapping below
+	// TODO: ADDING parameters to the method mapping below
 	[EMPLOYEESAPI.REMOVE]() {
-		return removeEmployee(Employees,);
+		return removeEmployee(Employees);
 	},
 	[EMPLOYEESAPI.REMOVE_CREATED_TASK]() {
-		return removeCreateTaskFromEmployee(Employees, );
+		return removeCreateTaskFromEmployee(Employees);
 	},
 	[EMPLOYEESAPI.REMOVE_ASSIGNED_TASK]() {
-		return removeAssignedTaskFromExployee(Employees, );
+		return removeAssignedTaskFromExployee(Employees);
 	},
 	[EMPLOYEESAPI.REMOVE_WATCHED_TASK]() {
-		return removeWatchedTaskFromEmployee(Employees,);
+		return removeWatchedTaskFromEmployee(Employees);
 	},
 	[EMPLOYEESAPI.CREATE_TASK]() {
-		return createTask(Employees, );
+		return createTask(Employees);
 	},
 	[EMPLOYEESAPI.ASSIGN_TASK]() {
-		return assignTaskToEmployee(Employees, );
+		return assignTaskToEmployee(Employees);
 	},
 	[EMPLOYEESAPI.WATCH_TASK]() {
-		return watchTask(Employees, );
+		return watchTask(Employees);
 	},
 	[EMPLOYEESAPI.UNWATCH_TASK]() {
-		return unwatchTask(Employees, );
+		return unwatchTask(Employees);
 	},
 });
-
-function newFunction() {
-	return this.userId;
-}
