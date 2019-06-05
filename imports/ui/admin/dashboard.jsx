6f6147304f89aeabb7236e7ca315af5bf3ec7bd5
Nero Cui <neroc@ensemble.com>
2019-06-05 10:25:01 -0700

    Updated meteor to a newer version, tried to add some UI.

--- imports/ui/admin/dashboard.jsx
+++ imports/ui/admin/dashboard.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { Stack, IStackProps } from "office-ui-fabric-react/lib/Stack";
+import { TextField, MaskedTextField } from "office-ui-fabric-react/lib/TextField";
+import { css, classNamesFunction, IButtonProps, IStyle, PrimaryButton } from "office-ui-fabric-react";
 
 export default class AdminDashboard extends Component {
 	constructor(props) {
@@ -10,10 +13,19 @@ export default class AdminDashboard extends Component {
 
 	render() {
 		return (
-			<div>
-				<h1>{ this.state.tab }</h1>
-				Admin dashboard
-			</div>
+			<Stack horizontal>
+				<h1>{this.state.tab}</h1>
+				<form>
+					<Stack>
+						<TextField label="Email" />
+						<TextField label="Password" type="password" />
+						<PrimaryButton ariaDescription="Button description for accessibility.">
+							Create Account
+						</PrimaryButton>
+					</Stack>
+				</form>
+
+			</Stack>
 		);
 	}
 }
