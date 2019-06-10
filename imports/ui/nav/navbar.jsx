621da23da21ee5e11283cd341266b12d24f0f509
Nero Cui <nerocui@outlook.com>
2019-06-10 13:16:29 -0700

    added simple nav for easy testing.

--- imports/ui/nav/navbar.jsx
+++ imports/ui/nav/navbar.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => <div>Navigation Bar</div>;
+const Navbar = () => (
+	<div>
+		<h1>Navbar</h1>
+		<ul>
+			<li>
+				<Link to="/">Home</Link>
+			</li>
+			<li>
+				<Link to="/admin">Admin Dashboard</Link>
+			</li>
+			<li>
+				<Link to="/dashboard">Employee Dashboard</Link>
+			</li>
+		</ul>
+		
+	</div>
+);
 
 export default Navbar;
