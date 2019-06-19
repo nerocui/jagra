a567dfd8c999a57382a030464fcbd16f56ae1a43
klnN <sentsuki8@gmail.com>
2019-06-18 21:38:47 -0700

    add reset

--- imports/ui/admin/dashboard.jsx
+++ imports/ui/admin/dashboard.jsx
@@ -6,25 +6,31 @@ import { signup } from "../../util/authUtil";
 import EmployeeList from "../employee/employeeList.jsx";
 import { Employees } from "../../api/db";
 
-class AdminDashboard extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
+const initialState = {
 			tab: "dashboard",
 			email: "",
 			firstName: "",
 			lastName: "",
-		};
+};
+
+class AdminDashboard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = initialState;
 		this.onSubmit = this.onSubmit.bind(this);
 		this.onEmailChange = this.onEmailChange.bind(this);
 		this.onFirstnameChange = this.onFirstnameChange.bind(this);
 		this.onLastnameChange = this.onLastnameChange.bind(this);
+		this.reset = this.reset.bind(this);
 	}
+
 
 	onSubmit(e) {
 		e.preventDefault();
 		console.log("hello");
 		signup(this.state.email, this.state.firstName, this.state.lastName);
+		console.log(`before reset: the name is ${ this.state.firstName }`);
+		this.reset();
 	}
 
 	onEmailChange(e) {
@@ -39,6 +45,12 @@ class AdminDashboard extends Component {
 		this.setState({ lastName: e.target.value.trim() });
 	}
 
+	reset() {
+		this.setState({ initialState }, () => {
+			console.log(`after reset, the state is ${ JSON.stringify(initialState) }`);
+		});
+	}
+
 	render() {
 		return (
 			<div>
@@ -50,6 +62,9 @@ class AdminDashboard extends Component {
   onEmailChange={this.onEmailChange}
   onFirstnameChange={this.onFirstnameChange}
   onLastnameChange={this.onLastnameChange}
+  emailValue={this.state.email}
+  firstNameValue={this.state.firstName}
+  lastNameValue={this.state.lastName}
 				/>
 				<EmployeeList employees={this.props.employeeList} />
 			</div>
