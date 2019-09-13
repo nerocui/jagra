d5cb92553777c066b2156b605aae8ab272fd342e
Nero Cui <nerocui@outlook.com>
2019-09-13 11:08:28 -0700

    revised login logic and ui

--- client/main.jsx
+++ client/main.jsx
@@ -1,21 +1,17 @@
 /* eslint-disable meteor/no-session */
+import React from "react";
 import { Accounts } from "meteor/accounts-base";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 import { createBrowserHistory } from "history";
 import { Tracker } from "meteor/tracker";
 import { Session } from "meteor/session";
-import routes from "../imports/ui/routes/index.jsx";
+import Routes from "../imports/ui/routes/index.jsx";
 
 const history = createBrowserHistory();
 
 Meteor.startup(() => {
-	render(routes, document.getElementById("react-target"));
-	Tracker.autorun(() => {
-		if (!Meteor.userId()) {
-			history.push("/");
-		}
-	});
+	render(<Routes />, document.getElementById("react-target"));
 });
 
 Accounts.onEnrollmentLink((token, done) => {
